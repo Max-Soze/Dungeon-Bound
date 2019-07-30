8b514a129d0f42e9d4f1f977867ead05be71a249
Max-Soze <maximilian.kaiser22@gmail.com>
2019-07-30 10:45:09 -0700

    Boundaries set and savegame working for position

--- dungeonbound.py
+++ dungeonbound.py
@@ -11,12 +11,11 @@ WINDOWHEIGHT = 900
 CELLSIZE = 50
 
 # Colors
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 #Other
-DATA = {}
+DATA = {"playerX":0, "playerY":0}
 
 def main():
     global FPSCLOCK, DISPLAYSURF, ALPHAFONT
@@ -41,9 +40,20 @@ def main():
         #gameOver
 
 def runGame():
+    global DATA
+    
     # set up game
     direction = None
+    saving = False
+    posDat = {}
+    mouseX = 0
+    mouseY = 0
+    upds = ()
+
+    #set up character
     player = Character(50, 50)
+    player.xPos = DATA['playerX']
+    player.yPos = DATA['playerY']
 
     #testing
     test = RoomMap("floor")
@@ -52,10 +62,10 @@ def runGame():
         #event handler
         for event in pygame.event.get():
             if event.type == QUIT:
-                terminate()
+                terminate(upds)
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    terminate()
+                    terminate(upds)
                 elif event.key == K_d:
                     direction = "right"
                 elif event.key == K_a:
@@ -64,8 +74,18 @@ def runGame():
                     direction = "up"
                 elif event.key == K_s:
                     direction = "down"
+                elif event.key == K_p:
+                    saving = True
         
         #movement controller
+        if player.xPos == 0 and direction == 'left':
+            direction = None
+        if player.xPos == 9 and direction == 'right':
+            direction = None
+        if player.yPos == 0 and direction == 'up':
+            direction = None
+        if player.yPos == 9 and direction == 'down':
+            direction = None
         player.move(direction)
         direction = None
 
@@ -76,6 +96,12 @@ def runGame():
         drawChar(player)
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+
+        #saves
+        if saving == True:
+            saving = False
+            posDat.update({'playerX':player.xPos, 'playerY':player.yPos})
+            upds = (posDat,)
 
 
 def drawChar(char):
@@ -90,13 +116,15 @@ def drawRoom(room):
             DISPLAYSURF.blit(room[x][y].image, room[x][y].rect)
 
 
-def terminate():
-    saveDat()
+def terminate(upds):
+    saveDat(upds)
     pygame.quit()
     sys.exit()
 
-def saveDat():
+def saveDat(upds):
     global DATA
+    for save in upds:
+        DATA.update(save)
     with open("src/savegame.json", "w") as save:
         json.dump(DATA, save)
 
